7b5815997d6c2419a0e07d126fdd8ae5ef1ac45c
JJudeApolonio <justinejudeapolonio@gmail.com>
2025-04-03 10:35:57 +0800

    Update activity3.js

--- LA1_APOLONIO_CS1D/activity3.js
+++ LA1_APOLONIO_CS1D/activity3.js
@@ -1,7 +1,7 @@
 /*boolean Activity
 isJsProgrammingLanguage = true  //variables with boolean values
 isHard = false  
-favnumb = 24 // same from activity 2, variable and i stored na number.
+favnumb = 24 // same activity 2, variable and i stored a number.
 add = favnumb + undefined  add with undefined 
 console.log("Is Js Programming Languange?",isJsProgrammingLanguage,",Is it Hard?",isHard)
 console.log("my favorite number is", favnumb, "when added with undefined:",add)

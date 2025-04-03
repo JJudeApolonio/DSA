e798bccce026d231e1c9de821240a2eb8dabdf2a
JJudeApolonio <justinejudeapolonio@gmail.com>
2025-04-03 10:09:45 +0800

    Update activity2.js

--- LA1_APOLONIO_CS1D/activity2.js
+++ LA1_APOLONIO_CS1D/activity2.js
@@ -1,1 +1,14 @@
 
+/*Numbers Activity 
+//variable for 2 numbers and stored 2 numbers.
+firstFavNum = 34;
+secondFavNum = 35;
+// variable for each operation 
+addition =(firstFavNum + secondFavNum);
+subtraction =(firstFavNum - secondFavNum);
+multiplication =(firstFavNum * secondFavNum);
+divition = (firstFavNum / secondFavNum);
+Modulu = (firstFavNum % secondFavNum);
+Exponentiation = (firstFavNum ** secondFavNum);
+console.log("Addition = ", addition,",Subtraction = ", subtraction,",Multiplication = ", multiplication,",Division = ", divition,",Modulus = ", Modulu,",Exponentiation = ", Exponentiation)
+//  called the variables 

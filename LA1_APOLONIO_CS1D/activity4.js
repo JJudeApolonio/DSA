84fac2536beb48b7f9336443996e3e2a849725c6
JJudeApolonio <justinejudeapolonio@gmail.com>
2025-04-03 10:14:55 +0800

    Update activity4.js

--- LA1_APOLONIO_CS1D/activity4.js
+++ LA1_APOLONIO_CS1D/activity4.js
@@ -1,1 +1,21 @@
+/*comparison or relational opereator
+
+firstFavNumb =5;
+secondFavNumb =69;
+
+greaterthan = (firstFavNumb > secondFavNumb); 
+lessthan = (firstFavNumb < secondFavNumb);
+greaterthan_or_equal_to = (firstFavNumb >= secondFavNumb); 
+strict_equality = (firstFavNumb === secondFavNumb);
+loose_equality = (firstFavNumb == secondFavNumb);
+strict_inequality = (firstFavNumb !== secondFavNumb);//  
+loose_ineguality = (firstFavNumb != secondFavNumb);/
+console.log(greaterthan);
+console.log(lessthan);
+console.log(greaterthan_or_equal_to);
+console.log(lessthan_or_equal_to);
+console.log(strict_equality);
+console.log(loose_equality);
+console.log(strict_inequality);
+console.log(loose_ineguality);
 

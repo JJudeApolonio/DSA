931a47af03a86d2d65731710e17c64be1dd08c1b
JJudeApolonio <justinejudeapolonio@gmail.com>
2025-04-03 10:38:02 +0800

    Update activity5.js

--- LA1_APOLONIO_CS1D/activity5.js
+++ LA1_APOLONIO_CS1D/activity5.js
@@ -1,8 +1,8 @@
 /*strings 
 
-favActorfirstName = "Ervin John";
-favActorlastName = "Calvo";
-fullName = favActorfirstName +" "+ favActorlastName; // concatenate the first and last name to make a full name  
+favActorfirstName = "Justine Jude";
+favActorlastName = "Apolonio";
+fullName = favActorfirstName +" "+ favActorlastName; //first and last name to make a full name  
 uppercase = fullName.toUpperCase(); //  uppercase
 message = "`My favorite Actor Is " + uppercase + " his best movie is Deadpool"; // just added some strings to make a sentence.
 console.log(fullName);

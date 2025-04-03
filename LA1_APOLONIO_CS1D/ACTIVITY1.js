a0a0a62da285d0b46da9facb32576d54a2bbd4b7
JJudeApolonio <justinejudeapolonio@gmail.com>
2025-04-03 10:06:59 +0800

    Update ACTIVITY1.js

--- LA1_APOLONIO_CS1D/ACTIVITY1.js
+++ LA1_APOLONIO_CS1D/ACTIVITY1.js
@@ -1,1 +1,9 @@
 
+//variables activity
+
+name = "jude"; // i have variable "name" and i am storing my name.
+whatDoYouwannaBecomeInYourLife = "programmer"; // same here, variable "whatDoYouwannaBecomeInYourLife" and i stored programmer.
+gender = "male"; // variable gender and i am a male.
+TWITTERHandle = "Justine JUDE"; // same-same here var facebookhandle and i stored my facebook account name.
+console.log("My Name is", name, "i want to become a" , whatDoYouwannaBecomeInYourLife, " and my facebook is", facebookHandle);
+ //  print my stored variables.i just added some string to make a sentence
